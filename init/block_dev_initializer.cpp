87962eba818dfd6550bf3afa1e5d6c3a21f18055
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-24 20:15:55 +0800

    block_dev_initializer: Find block device name in boot devices as fallback
    * Fixes issues like /dev/block/sda1 not appearing if it has
      uevent.partition_name and fstab wants /dev/block/sda1 but not
      by-name symlink to the partition
    
    Change-Id: I86105f36b031ba423a7bec70ed37db0c4388f57e

--- init/block_dev_initializer.cpp
+++ init/block_dev_initializer.cpp
@@ -120,13 +120,12 @@ ListenerAction BlockDevInitializer::HandleUevent(const Uevent& uevent,
     }
 
     auto name = uevent.partition_name;
-    if (name.empty()) {
-        size_t base_idx = uevent.path.rfind('/');
-        if (base_idx == std::string::npos) {
-            return ListenerAction::kContinue;
-        }
-        name = uevent.path.substr(base_idx + 1);
+
+    size_t base_idx = uevent.path.rfind('/');
+    if (base_idx == std::string::npos) {
+        return ListenerAction::kContinue;
     }
+    auto block_device_name = uevent.path.substr(base_idx + 1);
 
     auto iter = devices->find(name);
     if (iter == devices->end()) {
@@ -135,7 +134,11 @@ ListenerAction BlockDevInitializer::HandleUevent(const Uevent& uevent,
             iter = devices->find(partition_name);
         }
         if (iter == devices->end()) {
-            return ListenerAction::kContinue;
+            name = block_device_name;
+            iter = devices->find(name);
+            if (iter == devices->end()) {
+                return ListenerAction::kContinue;
+            }
         }
     }
 
