6c91192941391ee1a356e4c525726d5beb2a6a59
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-02 18:29:21 +0000

    property_service: Load /system/etc/override.prop
    Change-Id: I355b23b433c50b38d8e14b66adddedd3561f8628

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -76,6 +76,8 @@
 #include "util.h"
 #include "vendor_init.h"
 
+static constexpr char kOverrideProp[] = "/system/etc/override.prop";
+
 static constexpr char APPCOMPAT_OVERRIDE_PROP_FOLDERNAME[] =
         "/dev/__properties__/appcompat_override";
 static constexpr char APPCOMPAT_OVERRIDE_PROP_TREE_FILE[] =
@@ -1227,6 +1229,14 @@ void PropertyLoadBootDefaults() {
     if (access(kDebugRamdiskProp, R_OK) == 0) {
         LOG(INFO) << "Loading " << kDebugRamdiskProp;
         if (auto res = load_properties_from_file(kDebugRamdiskProp, nullptr, &properties);
+            !res.ok()) {
+            LOG(WARNING) << res.error();
+        }
+    }
+
+    if (access(kOverrideProp, R_OK) == 0) {
+        LOG(INFO) << "Loading " << kOverrideProp;
+        if (auto res = load_properties_from_file(kOverrideProp, nullptr, &properties);
             !res.ok()) {
             LOG(WARNING) << res.error();
         }
