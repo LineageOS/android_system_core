5561d3654d5dd27090779bb78fc91ace10babf12
Hridaya Prajapati <me@hridaya.dev>
2026-08-10 15:21:27 +0545

    healthd: Rescan for chargers
    Change-Id: I7c668922cb301b4d797d8b25bc1057d5a4296ea7

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -431,6 +431,16 @@ static BatteryHealthData* ensureBatteryHealthData(HealthInfo* info) {
 }
 
 void BatteryMonitor::updateValues(void) {
+    std::unique_ptr<DIR, decltype(&closedir)> dir(opendir(POWER_SUPPLY_SYSFS_PATH), closedir);
+    if (dir != NULL) {
+        struct dirent* entry;
+        while ((entry = readdir(dir.get()))) {
+            const char* name = entry->d_name;
+            if (!strcmp(name, ".") || !strcmp(name, "..")) continue;
+            updateChargerPresence(name);
+        }
+    }
+
     initHealthInfo(mHealthInfo.get());
 
     if (!mHealthdConfig->batteryPresentPath.empty())
