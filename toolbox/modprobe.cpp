miyu-xu/system-core

toolbox/modprobe: Add parallel option to load modules in parallel
Check the parallel option to determine
whether to call LoadListedModules or LoadModulesParallel.

The option applies when modules_load_file exists.

Signed-off-by: Jaehyoung Choi <jkkkkk.choi@samsung.com>
Change-Id: I541e656f721f5e4c97acae442c597f4e4ae61a58

toolbox/modprobe.cpp
@@ -29,6 +29,7 @@
 #include <modprobe/modprobe.h>
 
 #include <sys/utsname.h>
+#include <thread>
 
 namespace {
 
@@ -56,6 +57,7 @@ void print_usage(void) {
     LOG(INFO) << "  -s, --syslog: print to syslog also";
     LOG(INFO) << "  -q, --quiet: disable messages";
     LOG(INFO) << "  -v, --verbose: enable more messages, even more with a second -v";
+    LOG(INFO) << "  -p, --parallel: Load modules in parallel";
     LOG(INFO);
 }
 
@@ -157,7 +159,7 @@ extern "C" int modprobe_main(int argc, char** argv) {
     std::string mods;
     std::vector<std::string> mod_dirs;
     modprobe_mode mode = AddModulesMode;
-    bool blocklist = false;
+    bool blocklist = false, parallel = false;
     int rv = EXIT_SUCCESS;
 
     int opt, fd;
@@ -176,9 +178,10 @@ extern "C" int modprobe_main(int argc, char** argv) {
         { "remove",              no_argument,       0, 'r' },
         { "syslog",              no_argument,       0, 's' },
         { "verbose",             no_argument,       0, 'v' },
+        { "parallel",            no_argument,       0, 'p' },
     };
     // clang-format on
-    while ((opt = getopt_long(argc, argv, "a::bd:Dhlqrsv", long_options, &option_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "a::bd:Dhlqrsvp", long_options, &option_index)) != -1) {
         switch (opt) {
             case 'a':
                 // toybox modprobe supported -a to load multiple modules, this
@@ -233,6 +236,9 @@ extern "C" int modprobe_main(int argc, char** argv) {
                 } else {
                     android::base::SetMinimumLogSeverity(android::base::DEBUG);
                 }
+                break;
+            case 'p':
+                parallel = true;
                 break;
             default:
                 LOG(ERROR) << "Unrecognized option: " << opt;
@@ -300,7 +306,10 @@ extern "C" int modprobe_main(int argc, char** argv) {
 
     Modprobe m(mod_dirs, modules_load_file.empty() ? "modules.load" : modules_load_file, blocklist);
     if (mode == AddModulesMode && !modules_load_file.empty()) {
-        if (!m.LoadListedModules(false)) {
+        bool retval = (parallel) ? m.LoadModulesParallel(std::thread::hardware_concurrency())
+                                 : m.LoadListedModules(false);
+
+        if (!retval) {
             PLOG(ERROR) << "Failed to load all the modules from " << modules_load_file;
             return EXIT_FAILURE;
         }

LineageOS/android_system_core

Bug: 404353675

toolbox/modprobe.cpp
@@ -20,6 +20,7 @@
 #include <unistd.h>
 
 #include <string>
+#include <thread>
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -52,6 +53,7 @@ void print_usage(void) {
     LOG(INFO) << "  -D, --show-depends: Print dependencies for modules only, do not load";
     LOG(INFO) << "  -h, --help: Print this help";
     LOG(INFO) << "  -l, --list: List modules matching pattern";
+    LOG(INFO) << "  -p, --parallel: Load modules in parallel";
     LOG(INFO) << "  -r, --remove: Remove MODULE (multiple modules may be specified)";
     LOG(INFO) << "  -s, --syslog: print to syslog also";
     LOG(INFO) << "  -q, --quiet: disable messages";
@@ -157,7 +159,7 @@ extern "C" int modprobe_main(int argc, char** argv) {
     std::string mods;
     std::vector<std::string> mod_dirs;
     modprobe_mode mode = AddModulesMode;
-    bool blocklist = false;
+    bool blocklist = false, parallel = false;
     int rv = EXIT_SUCCESS;
 
     int opt, fd;
@@ -172,13 +174,14 @@ extern "C" int modprobe_main(int argc, char** argv) {
         { "show-depends",        no_argument,       0, 'D' },
         { "help",                no_argument,       0, 'h' },
         { "list",                no_argument,       0, 'l' },
+        { "parallel",            no_argument,       0, 'p' },
         { "quiet",               no_argument,       0, 'q' },
         { "remove",              no_argument,       0, 'r' },
         { "syslog",              no_argument,       0, 's' },
         { "verbose",             no_argument,       0, 'v' },
     };
     // clang-format on
-    while ((opt = getopt_long(argc, argv, "a::bd:Dhlqrsv", long_options, &option_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "a::bd:Dhlpqrsv", long_options, &option_index)) != -1) {
         switch (opt) {
             case 'a':
                 // toybox modprobe supported -a to load multiple modules, this
@@ -216,6 +219,9 @@ extern "C" int modprobe_main(int argc, char** argv) {
             case 'l':
                 check_mode();
                 mode = ListModulesMode;
+                break;
+            case 'p':
+                parallel = true;
                 break;
             case 'q':
                 android::base::SetMinimumLogSeverity(android::base::WARNING);
@@ -300,7 +306,10 @@ extern "C" int modprobe_main(int argc, char** argv) {
 
     Modprobe m(mod_dirs, modules_load_file.empty() ? "modules.load" : modules_load_file, blocklist);
     if (mode == AddModulesMode && !modules_load_file.empty()) {
-        if (!m.LoadListedModules(false)) {
+        bool retval = (parallel) ? m.LoadModulesParallel(std::thread::hardware_concurrency())
+                                 : m.LoadListedModules(false);
+
+        if (!retval) {
             PLOG(ERROR) << "Failed to load all the modules from " << modules_load_file;
             return EXIT_FAILURE;
         }